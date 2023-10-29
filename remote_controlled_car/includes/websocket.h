041e120f08cd9975724c2acb87828450d68d8b73
goddfatherr <asogwaemmanuel36@gmail.com>
2023-10-29 21:35:28 +0530

    refactored

--- remote_controlled_car/includes/websocket.h
+++
@@ -1,6 +0,0 @@
-#ifndef WEBSOCKET_H
-#define WEBSOCKET_H
-
-httpd_handle_t start_webserver(void);
-
-#endif
--- ws_server/main/ws_echo_server.c
+++ ws_server/main/ws_echo_server.c
@@ -1,12 +1,3 @@
-/* WebSocket Echo Server Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-
 #include <esp_wifi.h>
 #include <esp_event.h>
 #include <esp_log.h>
@@ -18,8 +9,6 @@
 #include "protocol_examples_common.h"
 
 #include <esp_http_server.h>
-
-#include "driver.h"
 
 /* A simple example that demonstrates using websocket echo server
  */
@@ -66,7 +55,7 @@ static esp_err_t trigger_async_send(httpd_handle_t handle, httpd_req_t *req)
  * This handler echos back the received ws data
  * and triggers an async send if certain message received
  */
-static esp_err_t controller_handler(httpd_req_t *req)
+static esp_err_t echo_handler(httpd_req_t *req)
 {
     if (req->method == HTTP_GET) {
         ESP_LOGI(TAG, "Handshake done, the new connection was opened");
@@ -102,39 +91,29 @@ static esp_err_t controller_handler(httpd_req_t *req)
     }
     ESP_LOGI(TAG, "Packet type: %d", ws_pkt.type);
     if (ws_pkt.type == HTTPD_WS_TYPE_TEXT &&
-        strcmp((char*)ws_pkt.payload,"goStraight") == 0) {
-        driverGoStraight(); 
+        strcmp((char*)ws_pkt.payload,"Trigger async") == 0) {
+        free(buf);
+        return trigger_async_send(req->handle, req);
     }
-    else if (ws_pkt.type == HTTPD_WS_TYPE_TEXT &&
-        strcmp((char*)ws_pkt.payload,"goBack") == 0) {
-        driverGoStraight();
-    }
-    else if (ws_pkt.type == HTTPD_WS_TYPE_TEXT &&
-        strcmp((char*)ws_pkt.payload,"turnLeft") == 0) {
-        driverGoStraight(); 
-    }
-    else if (ws_pkt.type == HTTPD_WS_TYPE_TEXT &&
-        strcmp((char*)ws_pkt.payload,"turnRight") == 0) {
-        driverGoStraight();
-    }
-    else{
-        driverStop();
+
+    ret = httpd_ws_send_frame(req, &ws_pkt);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "httpd_ws_send_frame failed with %d", ret);
     }
     free(buf);
-    return ESP_OK;
-
+    return ret;
 }
 
 static const httpd_uri_t ws = {
         .uri        = "/ws",
         .method     = HTTP_GET,
-        .handler    = controller_handler,
+        .handler    = echo_handler,
         .user_ctx   = NULL,
         .is_websocket = true
 };
 
 
-httpd_handle_t start_webserver(void)
+static httpd_handle_t start_webserver(void)
 {
     httpd_handle_t server = NULL;
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
@@ -183,3 +162,32 @@ static void connect_handler(void* arg, esp_event_base_t event_base,
 }
 
 
+void app_main(void)
+{
+    static httpd_handle_t server = NULL;
+
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+    ESP_ERROR_CHECK(example_connect());
+
+    /* Register event handlers to stop the server when Wi-Fi or Ethernet is disconnected,
+     * and re-start it upon connection.
+     */
+#ifdef CONFIG_EXAMPLE_CONNECT_WIFI
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &connect_handler, &server));
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &disconnect_handler, &server));
+#endif // CONFIG_EXAMPLE_CONNECT_WIFI
+#ifdef CONFIG_EXAMPLE_CONNECT_ETHERNET
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_ETH_GOT_IP, &connect_handler, &server));
+    ESP_ERROR_CHECK(esp_event_handler_register(ETH_EVENT, ETHERNET_EVENT_DISCONNECTED, &disconnect_handler, &server));
+#endif // CONFIG_EXAMPLE_CONNECT_ETHERNET
+
+    /* Start the server for the first time */
+    server = start_webserver();
+}
